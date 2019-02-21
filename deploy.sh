63880e4f867d26b659bdf93b8d7675848031bfd0
mishal.shah <mishal.shah@sophos.com>
2019-02-21 14:02:05 +0530

    Adding terraform.

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+DEPLOYMENT_ENV=$1
+
+if [ "$DEPLOYMENT_ENV" == "" ]; then
+    echo "INFO: No DEPLOYMENT_ENV provided."
+    exit 0
+fi
+
+
+source deploy/variables/${DEPLOYMENT_ENV}.properties
+
+## Installing Terraform
+
+if [ "$DEPLOYMENT_ENV" == "local" ]; then
+    sh deploy/scripts/deploy_local.sh ${TF_PATH} $AWS_REGIONS
+elif [ "$DEPLOYMENT_ENV" == "prod" ]; then
+    sh deploy/scripts/deploy_prod.sh ${TF_PATH} $AWS_REGIONS
+fi
